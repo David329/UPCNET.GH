9418475ecc38a05c1983382e9236d9b076c926bc
Vincenzo <vincenzogreggio@hotmail.com>
2016-05-04 14:49:32 -0500

    Se añade Diagrama clases y Cliente Web

---
+++ Proyecto/UPCNETSystemCliente2/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPCNETSystemCliente.UPCNETSystemWebService;
+
+namespace UPCNETSystemCliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            UPCNETServiceClient proxy = new UPCNETServiceClient();
+            UPCNETSystemWebService.alumno[] Ian = proxy.Enviar_Alumnos();
+            return View();
+        }
+
+    }
+}
