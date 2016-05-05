d5a1e67f523e535e09b0b6c55ce8cc1cf50e6d99
Vincenzo <vincenzogreggio@hotmail.com>
2016-05-04 21:41:40 -0500

    Se agregó la clase alumno en BE, se eliminó el proyecto W.Form y se implementó la vista y display de la lista de Alumnos en html

---
+++ Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/UPCNETSystemCliente/Business Entities/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UPCNETSystemCliente.Business_Entities
+{
+    public class Alumno
+    {
+       
+        private String Nombre;
+        private String Apellido;
+        private String Codigo;
+        private String Carrera;
+        private String DNI;
+        private String Correo;
+        private String Fecha;
+        private String IDCategoria;
+
+        public Alumno(String nombre, String apellido, String codigo, String carrera, String dni, String correo,
+                 String fecha, String idCategoria)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Codigo = codigo;
+            this.Carrera = carrera;
+            this.DNI = dni;
+            this.Correo = correo;
+            this.Fecha = fecha;
+            this.IDCategoria = idCategoria;
+        }
+        public String getNombre() { return Nombre;}
+        public String getApellido() { return Apellido; }
+        public String getCodigo() { return Codigo; }
+        public String getCarrera() { return Carrera; }
+        public String getDNI() { return DNI; }
+        public String getCorreo() { return Correo; }
+        public String getFecha() { return Fecha; }
+        public String getIDCategoria() { return IDCategoria; }
+
+
+
+
+    }
+        
+   
+}
---
+++ Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UPCNETSystemCliente.UPCNETSystemWebService;
+using UPCNETSystemCliente.Business_Entities;
+
+namespace UPCNETSystemCliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            UPCNETServiceClient proxy = new UPCNETServiceClient();
+            UPCNETSystemWebService.alumno[] Ian = proxy.Enviar_Alumnos();
+            return View();
+        }
+
+        public ActionResult Alumnos()
+        {
+            // Reemplazar por la lista de Base de Datos
+            // Debo pedir permiso para implementar un template
+            List<Alumno> lista = new List<Alumno>();
+            lista.Add(new Alumno("Ian","Maguiña","666","Ing. de Bica","666","La bica.com","Fecha","00"));
+            lista.Add(new Alumno("Victor","Weaver","777","Ing. de Ian","999","asd.com","Fecha 2","01"));
+            lista.Add(new Alumno("Ulices", "NoConS", "888", "Ing. de Ian", "990", "asds.com", "Fecha 3", "03"));
+            lista.Add(new Alumno("Peppi", "Alex", "787", "Ing. de Ian", "299", "as2.com", "Fecha 4", "02"));
+            return View(lista);
+        }
+
+    }
+}
