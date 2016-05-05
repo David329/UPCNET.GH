efb81b2363415a5998ac61384a015744fbee40cf
Vincenzo <vincenzogreggio@hotmail.com>
2016-05-04 23:05:45 -0500

    Se añadieron opciones de navegación básicas

--- Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/UPCNETSystemCliente/App_Start/RouteConfig.cs
+++ Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/UPCNETSystemCliente/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UPCNETSystemCliente
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Alumnos", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
