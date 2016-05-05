cf1a6d0b2a91b5bc9e6bcb5d1801980aa95f0011
Ulices Melendez <ulices009@hotmail.com>
2016-05-05 00:17:19 -0500

    Se agrego metodo editAlumno a la clase AlumnoDAO

--- Proyecto/UPCNETSystemWebService/src/java/com/upc/upcnet/dao/AlumnoDAO.java
+++ Proyecto/UPCNETSystemWebService/src/java/com/upc/upcnet/dao/AlumnoDAO.java
@@ -22,12 +22,12 @@ public class AlumnoDAO {
     
     public List<Alumno> getAlumnos(){
         List<Alumno> alumnos = new ArrayList<>();
-        Connection con = null;
+        Connection cn = null;
         try{
-            con = AccesoDB.getConnection();
+            cn = AccesoDB.getConnection();
             StringBuilder query = new StringBuilder();
             query.append("SELECT IDAlumno, Nombre, Apellido, DNI, Edad, Correo, Direccion, IDCategoria, IDApoderado FROM Alumno");
-            PreparedStatement ps = con.prepareStatement(query.toString());
+            PreparedStatement ps = cn.prepareStatement(query.toString());
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
                 Alumno a = new Alumno();
@@ -49,29 +49,83 @@ public class AlumnoDAO {
             throw new RuntimeException("No se tiene acceso al servidor");
         }finally{
             try{
-                if(con != null)
-                    con.close();
-            }catch(Exception ex){
-            }
+                if(cn != null)
+                    cn.close();
+            }catch(Exception ex){}
         }
         return alumnos;
     }
     
-    public void setAlumno(String _idAlumno, String _nombre, String _apellido, String _dni, String _edad, String _correo, String _direccion, String idCategoria, String _idApoderado){
-        Connection con = null;        
+    public void setAlumno(String _idAlumno, String _nombre, String _apellido, String _dni, String _edad, String _correo, String _direccion, String _idCategoria, String _idApoderado){
+        Connection cn = null;        
         try{
-            con = AccesoDB.getConnection();
-            con.setAutoCommit(false);
+            cn = AccesoDB.getConnection();
+            cn.setAutoCommit(false);
             StringBuilder query = new StringBuilder();
             query.append("SELECT * FROM Alumno WHERE IDAlumno = ?");
-            PreparedStatement ps = con.prepareStatement(query.toString());
+            PreparedStatement ps = cn.prepareStatement(query.toString());
             ps.setString(1, _idAlumno);
             ResultSet rs = ps.executeQuery();
             if(rs.next())
                 throw new SQLException("El codigo del alumno ya existe");
             
-        }catch(Exception ex){
+            query = new StringBuilder();
+            query.append("INSERT INTO Alumno(IDAlumno, Nombre, Apellido, DNI, Edad, Correo, Direccion, IDCategoria, IDApoderado) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            ps = cn.prepareStatement(query.toString());
+            ps.setString(1, _idAlumno);
+            ps.setString(1, _nombre);
+            ps.setString(1, _apellido);
+            ps.setString(1, _dni);
+            ps.setString(1, _edad);
+            ps.setString(1, _correo);
+            ps.setString(1, _direccion);
+            ps.setString(1, _idCategoria);
+            ps.setString(1, _idApoderado);
+            ps.executeUpdate();
+            cn.commit();
             
+        }catch(SQLException ex){
+            throw new RuntimeException(ex.getMessage());
+        }catch(Exception e){
+            throw new RuntimeException("No se tiene acceso al servidor");
+        }finally{
+            try{
+                if(cn != null)
+                    cn.close();
+            }catch(Exception ex){}
         }
+    }
+    
+    public void editAlumno(String _idAlumno, String _nombre, String _apellido, String _dni, String _edad, String _correo, String _direccion, String _idCategoria, String _idApoderado){
+        Connection cn = null;
+        try{
+            cn = AccesoDB.getConnection();
+            cn.setAutoCommit(false);
+            StringBuilder query = new StringBuilder();
+            query.append("UPDATE Alumno SET Nombre = ?, Apellido = ?, DNI = ?, Edad = ?, Correo = ?, Direccion = ?, IDCategoria = ?, IDApoderado = ? WHERE IDAlumno = ?");
+            PreparedStatement ps = cn.prepareStatement(query.toString());
+            ps.setString(1, _nombre);
+            ps.setString(2, _apellido);
+            ps.setString(3, _dni);
+            ps.setString(4, _edad);
+            ps.setString(5, _correo);
+            ps.setString(6, _direccion);
+            ps.setString(7, _idCategoria);
+            ps.setString(8, _idApoderado);
+            ps.setString(9, _idAlumno);
+            int realizado = ps.executeUpdate();
+            if(realizado == 0)
+                throw  new SQLException("Alumno no existe!");            
+        }catch(SQLException ex){
+            throw new RuntimeException(ex.getMessage());
+        }catch(Exception e){
+            throw new RuntimeException("No se tiene acceso al servidor");
+        }finally{
+            try{
+                if(cn != null)
+                    cn.close();
+            }catch(Exception ex){}
+        }
+        
     }
 }
