6c9d25a1b0d520225dd4643ac4886d4b18c4e9b9
Ricardo Silva Manayalle <ricardo.silva.manayalle@gmail.com>
2016-05-06 23:14:42 -0500

    Nuevo template, controller y vistas

--- Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/HomeController.cs
+++ Proyecto/UPCNETSystemCliente/UPCNETSystemCliente/Controllers/HomeController.cs
@@ -3,34 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using UPCNETSystemCliente.UPCNETSystemWebService;
-using UPCNETSystemCliente.Business_Entities;
+using UPCNETSystemCliente.ViewModel.Login;
 
 namespace UPCNETSystemCliente.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        //
-        // GET: /Home/
 
         public ActionResult Index()
         {
-           // UPCNETServiceClient proxy = new UPCNETServiceClient();
-           // UPCNETSystemWebService.alumno[] listaAlumnos = proxy.Enviar_Alumnos();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login() {
+            _LoginViewModel objLoginViewModel = new _LoginViewModel();
+            return View(objLoginViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(_LoginViewModel objLoginViewModel)
+        {
             return View();
         }
 
-        public ActionResult Alumnos()
-        {
-            // Reemplazar por la lista de Base de Datos
-            // Debo pedir permiso para implementar un template
-            List<Alumno> listaAlumnos = new List<Alumno>();
-            listaAlumnos.Add(new Alumno("Al01","Ian", "Maguiña", "666", "Ing. de Bica", "666", "La bica.com", "Fecha", "00"));
-            listaAlumnos.Add(new Alumno("Al02", "Victor", "Weaver", "777", "Ing. de Ian", "999", "asd.com", "Fecha 2", "01"));
-            listaAlumnos.Add(new Alumno("Al03", "Ulices", "NoConS", "888", "Ing. de Ian", "990", "asds.com", "Fecha 3", "03"));
-            listaAlumnos.Add(new Alumno("Al04", "Peppi", "Alex", "787", "Ing. de Ian", "299", "as2.com", "Fecha 4", "02"));
-            listaAlumnos.Add(new Alumno("Al05", "Vincente", "Reaper", "123", "Ing. de Ian", "999", "asasd.com", "Fecha 5", "01"));
-            return View(listaAlumnos);
+        public ActionResult Principal_Administrador() {
+            return View();
         }
 
     }
